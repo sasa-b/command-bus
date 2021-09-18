8c8fd5a5a1f23721fdb4661df22852e01551c8a1
Saša Blagojević <sasa.blagojevic@mail.com>
2021-09-18 21:52:27 +0200

    Add .

---
+++ src/AbstractCommand.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sasa.blagojevic@mail.com
+ * Date: 18. 9. 2021.
+ * Time: 21:45
+ */
+
+declare(strict_types=1);
+
+namespace SasaB\CommandBus;
+
+abstract class AbstractCommand implements Command
+{
+
+}
