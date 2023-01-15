f774ef34b3bdaf580d3f09f388c87d545b6d4ffa
Sasa Blagojevic <sasa.blagojevic@mail.com>
2023-01-16 00:27:45 +0100

    Implement Message::payload on Command

--- src/Command.php
+++ src/Command.php
@@ -9,4 +9,10 @@ use SasaB\CommandBus\Concern\CanIdentify;
 abstract class Command implements Message
 {
     use CanIdentify;
+
+    public function payload(): mixed
+    {
+        // Override or ignore and use public read only properties
+        return null;
+    }
 }
